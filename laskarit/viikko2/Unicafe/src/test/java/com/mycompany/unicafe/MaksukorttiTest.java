9d300564517fb68ea59bbdbe76f1024a490b3187
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2020-03-15 16:50:22 +0200

    1-3 tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,50 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void SaldoAlussaOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(5000);
+        assertEquals("saldo: 60.0", kortti.toString());
+    }
+    
+    @Test
+    public void ottaminenVahentaaSaldoaOikein() {
+        kortti.otaRahaa(250);
+        assertEquals("saldo: 7.50", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRaahaEiTarpeeksi() {
+        kortti.otaRahaa(1250);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
+    @Test
+    public void ottaminenPalauttaaTrue() {
+        assertEquals(true,kortti.otaRahaa(250));
+    }
+    
+    @Test
+    public void ottaminenPalauttaaFalse() {
+        assertEquals(false,kortti.otaRahaa(1250));
+    }
+    
+    @Test
+    public void palauttaaSaldon() {
+        kortti.otaRahaa(250);
+        assertEquals(750,kortti.saldo());
     }
 }
